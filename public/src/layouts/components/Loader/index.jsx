121b19d9eefb954ed7bce0d33f15fc3cf977a936
montheramer <monther.amer48@gmail.com>
2021-05-07 18:51:42 +0300

    CRloader component

---
+++ public/src/layouts/components/Loader/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div className="loader-container">
+      <span className="loader-item">I</span>
+      <span className="loader-item">I</span>
+      <span className="loader-item">I</span>
+      <span className="loader-item">I</span>
+      <span className="loader-item">I</span>
+    </div>
+  );
+};
